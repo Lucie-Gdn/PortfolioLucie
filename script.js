5215366596b46caf73ab2bc0ed47411a5d7547a3
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-05-16 16:14:05 +0200

    portfolio stage 1 - structure and firsts projects

---
+++ script.js
@@ -0,0 +1,37 @@
+// Ligne sous le menu au scroll
+window.addEventListener("scroll", shade);
+
+function shade() {
+    if (window.scrollY>10){
+    let header =document.getElementById('header');
+    header.style.boxShadow = "#493d5f77 0px 1px 3px";
+    } else {
+    header.style.boxShadow = "";
+    }
+};
+
+// Souligner l'onglet actif du menu 
+
+let onglets = document.querySelectorAll('.onglet')
+
+const observer= new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+      console.log (entry.target.id, entry.isIntersecting) 
+        if (entry.isIntersecting){
+        onglets.forEach(onglet => {
+            onglet.classList.remove('active')
+            if(entry.target.id === onglet.dataset.nav){
+                onglet.classList.add('active')
+            }
+        });}
+    }); 
+},{
+    threshold:0.5
+})
+
+let sections= document.querySelectorAll('.section')
+sections.forEach( section => {
+    observer.observe(section);
+    });
+
+
